77cda7a3c7a8e92f7e860b8657ef4bebcdd5707a
WindaSitorus <windasitorus0197@gmail.com>
2024-10-09 16:12:42 +0700

    kami telah menyelesaikan tugas

--- X01.java
+++ X01.java
@@ -1,2 +1,38 @@
-// NIM - Your Name
-// NIM - Your Name
+// 12S24001-FredrickAritonang
+// 12S24019-Winda N.V Sitorus
+
+import java.util.*;
+import java.lang.Math;
+
+public class X01 {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        double hargabuku, total1, minimum, total2;
+
+        hargabuku = 0;
+        total1 = 0;
+        minimum = 1000;
+        do {
+            hargabuku = Double.parseDouble(input.nextLine());
+            if (hargabuku == 0) {
+            } else {
+                if (minimum < hargabuku) {
+                } else {
+                    minimum = hargabuku;
+                }
+                total1 = total1 + hargabuku;
+            }
+        } while (hargabuku != 0);
+        if (total1 >= 100) {
+            total2 = total1 - minimum;
+        } else {
+            total2 = minimum;
+        }
+        System.out.println(toFixed(total2,2));
+    }
+    
+    private static String toFixed(double value, int digits) {
+        return String.format("%." + digits + "f", value);
+    }
+}
